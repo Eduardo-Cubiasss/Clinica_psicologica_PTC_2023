c3488762885c54a9ca067fbb786d21feb20975d0
JP <juanpflapi@gmail.com>
2023-07-09 16:01:05 -0600

    update
    conexion sql y inicio luego del login creado :)

--- app/src/main/java/com/example/clinica_psicologicaa/Login.kt
+++ app/src/main/java/com/example/clinica_psicologicaa/Login.kt
@@ -4,26 +4,109 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 
 class Login : AppCompatActivity() {
+
+    private var conexionSQL = ConexionSQL()
+    private lateinit var Username: EditText
+    private lateinit var Password: EditText
+    private lateinit var btnIniciarSesion: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val btnTextRegister = findViewById<TextView>(R.id.btnRegistrar)
 
-        btnTextRegister.setOnClickListener{
+        btnTextRegister.setOnClickListener {
             val intent: Intent = Intent(this, Registrarse::class.java)
             startActivity(intent)
         }
 
         val btnTextRecordar = findViewById<TextView>(R.id.btnRecordarPassword)
 
-        btnTextRecordar.setOnClickListener{
+        btnTextRecordar.setOnClickListener {
             val intent: Intent = Intent(this, RecordarPassword::class.java)
             startActivity(intent)
-        }
-    }
 
+        }
+
+            /*
+
+                    Username = findViewById(R.id.Usuario)
+                    Password = findViewById(R.id.Contraseña)
+                    btnIniciarSesion = findViewById(R.id.btnIniciarSesion)
+
+                    btnIniciarSesion.setOnClickListener {
+                        val username = Username.text.toString()
+                        val password = Password.text.toString()
+
+                        if (validateLogin(username, password)) {
+                            // Inicio de sesión exitoso, redirigir a la siguiente actividad
+                            val intent = Intent(this, LoginInicio::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            // Mostrar cuadro de diálogo con mensaje de error
+                            val dialogBuilder = AlertDialog.Builder(this)
+                            dialogBuilder.setMessage("Datos ingresados incorrectos.")
+                                .setCancelable(false)
+                                .setPositiveButton("Aceptar") { dialog, _ ->
+                                    dialog.dismiss()
+                                }
+
+                            val alert = dialogBuilder.create()
+                            alert.setTitle("Error")
+                            alert.show()
+                        }
+                    }
+            }
+
+            private fun validateLogin(username: String, password: String): Boolean {
+                var connection: Connection? = null
+
+
+                val serverName = "192.168.0.9:50535"
+                val dbName = "dbMindLink"
+                val username = "user1"
+                val password = "user1"
+
+                try {
+                    // Establecer la conexión a la base de datos SQL Server
+                    Class.forName("net.sourceforge.jtds.jdbc.Driver")
+                    val url = "jdbc:jtds:sqlserver://$serverName/$dbName"
+                    connection = DriverManager.getConnection(url, username, password)
+
+                    // Realizar la consulta para validar los datos de inicio de sesión
+                    val query = conexionSQL.dbConn()?.prepareStatement("EXEC PDregistrarpacientes(?,?")!!
+                    query.setString(1, txtNombre.text.toString())
+                    query.setString(2, txtContraseña.text.toString())
+                    query.executeQuery()
+
+
+                } catch (e: SQLException) {
+                    e.printStackTrace()
+                } finally {
+                    // Cerrar la conexión
+                    try {
+                        connection?.close()
+                    } catch (e: SQLException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                return false // En caso de cualquier error o si no se encontraron registros
+
+                 //no estoy seguro de esto
+
+                 */
+
+
+    }
 }
--- app/src/main/java/com/example/clinica_psicologicaa/Registrarse.kt
+++ app/src/main/java/com/example/clinica_psicologicaa/Registrarse.kt
@@ -1,11 +1,31 @@
 package com.example.clinica_psicologicaa
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+
+lateinit var txtNombre : EditText
+lateinit var txtApellidos : EditText
+lateinit var txtNacimiento : EditText
+lateinit var txtIdClinica : EditText
+lateinit var txtContraseña : EditText
+lateinit var txtNumero : EditText
+lateinit var txtUsuario : EditText
+
 
 class Registrarse : AppCompatActivity() {
+
+
+    private var conexionSQL = ConexionSQL()
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
@@ -14,7 +34,42 @@ class Registrarse : AppCompatActivity() {
 
         btnTextOiniciarsesion.setOnClickListener{
             val intent: Intent = Intent(this, Login::class.java)
+
             startActivity(intent)
+
+            val Nombres = findViewById<EditText>(R.id.txtNombre)
+            val Apellidos = findViewById<EditText>(R.id.txtApellidos)
+            val FechaNacimiento = findViewById<EditText>(R.id.txtNacimiento)
+            val IdClinica = findViewById<EditText>(R.id.txtIdClinica)
+            val usuarios = findViewById<EditText>(R.id.txtUsuario)
+            val password = findViewById<EditText>(R.id.txtContraseña)
+            val Telefonos = findViewById<EditText>(R.id.txtNumero)
+
+            val txtCrearCuenta = findViewById<Button>(R.id.btnCrearCuenta)
+
+            txtCrearCuenta.setOnClickListener {
+                val Nombres = txtNombre.text.toString()
+                val Apellidos = txtApellidos.text.toString()
+                val FechaNacimiento = txtNacimiento.text.toString()
+                val IdClinica = txtIdClinica.text.toString()
+                val usuarios = txtUsuario.text.toString()
+                val password = txtContraseña.text.toString()
+                val Telefonos = txtNumero.text.toString()
+
+                val addPersona:PreparedStatement = conexionSQL.dbConn()?.prepareStatement("EXEC PDregistrarpacientes(?,?,?,?,?,?,?)")!!
+                addPersona.setString(1, txtNombre.text.toString())
+                addPersona.setString(2, txtApellidos.text.toString())
+                addPersona.setString(3, txtNacimiento.text.toString())
+                addPersona.setString(4, txtIdClinica.text.toString())
+                addPersona.setString(5, txtContraseña.text.toString())
+                addPersona.setString(7, txtNumero.text.toString())
+
+                addPersona.executeUpdate()
+
+            }
+
         }
+
     }
+
 }
