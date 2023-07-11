5d6ecaa13d09b4396b4eaea83e3d9c5340ef6d8e
ESTUDIANTE <kb73669@gmail.com>
2023-07-11 10:05:47 -0600

    Funciona el registrar usuarios

--- app/src/main/java/com/example/clinica_psicologicaa/Registrarse.kt
+++ app/src/main/java/com/example/clinica_psicologicaa/Registrarse.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.sql.PreparedStatement
-import java.util.Date
+import java.sql.SQLException
+import java.time.LocalDate
+import java.sql.Date
 
 
 lateinit var txtNombre : EditText
@@ -18,6 +21,7 @@ lateinit var txtIdClinica : EditText
 lateinit var txtContraseña : EditText
 lateinit var txtNumero : EditText
 lateinit var txtUsuario : EditText
+lateinit var btnCrearCuenta: ImageButton
 
 class Registrarse : AppCompatActivity() {
 
@@ -30,22 +34,25 @@ class Registrarse : AppCompatActivity() {
 
         val btnTextOiniciarsesion = findViewById<TextView>(R.id.btnOiniciarsesion)
 
-        btnTextOiniciarsesion.setOnClickListener{
+        btnTextOiniciarsesion.setOnClickListener {
             val intent: Intent = Intent(this, Login::class.java)
 
             startActivity(intent)
+        }
 
-            val Nombres = findViewById<EditText>(R.id.txtNombre)
-            val Apellidos = findViewById<EditText>(R.id.txtApellidos)
-            val FechaNacimiento = findViewById<EditText>(R.id.txtNacimiento)
-            val IdClinica = findViewById<EditText>(R.id.txtIdClinica)
-            val usuarios = findViewById<EditText>(R.id.txtUsuario)
-            val password = findViewById<EditText>(R.id.txtContraseña)
-            val Telefonos = findViewById<EditText>(R.id.txtNumero)
+            txtNombre = findViewById(R.id.txtNombre)
+            txtApellidos = findViewById(R.id.txtApellidos)
+            txtNacimiento = findViewById(R.id.txtNacimiento)
+            txtIdClinica = findViewById(R.id.txtIdClinica)
+            txtUsuario = findViewById(R.id.txtUsuario)
+            txtContraseña = findViewById(R.id.txtContraseña)
+            txtNumero = findViewById(R.id.txtNumero)
 
-            val txtCrearCuenta = findViewById<Button>(R.id.btnCrearCuenta)
+            btnCrearCuenta = findViewById(R.id.btnCrearCuenta)
 
-            txtCrearCuenta.setOnClickListener {
+            btnCrearCuenta.setOnClickListener {
+                print("boton funciona")
+
                 val Nombres = txtNombre.text.toString()
                 val Apellidos = txtApellidos.text.toString()
                 val FechaNacimiento = txtNacimiento.text.toString()
@@ -54,21 +61,27 @@ class Registrarse : AppCompatActivity() {
                 val password = txtContraseña.text.toString()
                 val Telefonos = txtNumero.text.toString()
 
-                val addPersona:PreparedStatement = conexionSQL.dbConn()?.prepareStatement("EXEC PDRegistrarpaciente(?,?,?,?,?,?,?)")!!
+                try {
+
+
+                val addPersona:PreparedStatement = conexionSQL.dbConn()?.prepareStatement("EXEC PDRegistrarpaciente ?,?,?,?,?,?,?" ) !!
+                print("Almacenar a la base funciona")
+
                 addPersona.setString(1, Nombres)
                 addPersona.setString(2, Apellidos)
-                addPersona.setString(3, FechaNacimiento)
+                addPersona.setDate(3, Date.valueOf(FechaNacimiento))
                 addPersona.setString(4, IdClinica)
                 addPersona.setString(5, usuarios)
                 addPersona.setString(6, password)
                 addPersona.setString(7, Telefonos)
 
                 addPersona.executeUpdate()
+                }catch (ex: SQLException){
+                    println("ESTE ES EL ERROR: ${ex.toString()}")
 
+                }
             }
 
         }
 
     }
-
-}
