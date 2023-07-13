e523b0b445c603ab7b4e631d205d7fa20877d986
dhakhax <af639012@gmail.com>
2023-07-12 21:21:50 -0600

    Inicio-diseño (falta completar bottom_nav_menu y programar)

--- app/src/main/java/com/example/clinica_psicologicaa/Inicio.kt
+++ app/src/main/java/com/example/clinica_psicologicaa/Inicio.kt
@@ -26,7 +26,7 @@ class Inicio : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_calendario, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
