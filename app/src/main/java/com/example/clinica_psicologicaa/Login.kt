b4f217c1bc0e7e88fda21290548a37a3352b40eb
DKlp <af639012@gmail.com>
2023-09-03 10:01:18 -0600

    cambio de juanpa login

--- app/src/main/java/com/example/clinica_psicologicaa/Login.kt
+++ app/src/main/java/com/example/clinica_psicologicaa/Login.kt
@@ -3,16 +3,23 @@ package com.example.clinica_psicologicaa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.clinica_psicologicaa.ui.dashboard.DashboardFragment
 import com.example.clinica_psicologicaa.ui.dashboard.DashboardViewModel
+import java.sql.CallableStatement
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 import java.sql.SQLException
+import java.sql.Types
 
 class Login : AppCompatActivity() {
 
@@ -24,6 +31,9 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Username = findViewById(R.id.Usuario)
+        Password = findViewById(R.id.Contraseña)
 
         val btnTextRegister = findViewById<TextView>(R.id.btnRegistrar)
 
@@ -40,83 +50,43 @@ class Login : AppCompatActivity() {
 
         }
 
-        val btnIniciarSesion: ImageButton = findViewById/*<ImageButton>*/(R.id.btnIniciarSesion)
-
+        val btnIniciarSesion: ImageButton = findViewById(R.id.btnIniciarSesion)
         btnIniciarSesion.setOnClickListener {
-            val intent: Intent = Intent(this@Login,Inicio::class.java)
-            startActivity(intent)
-            finish()
-        }
 
+            val conexionSQL = ConexionSQL().dbConn()
+            val procedureCall = "{call PDLogear(?, ?, ?, ?)}"
 
-        /*
-                    Username = findViewById(R.id.Usuario)
-                    Password = findViewById(R.id.Contraseña)
-                    btnIniciarSesion = findViewById(R.id.btnIniciarSesion)
+            val username = Username.text.toString() // Obtén el nombre de usuario ingresado
+            val password = Password.text.toString()
 
-                    btnIniciarSesion.setOnClickListener {
-                        val username = Username.text.toString()
-                        val password = Password.text.toString()
+            try {
+                val callableStatement: CallableStatement = conexionSQL?.prepareCall(procedureCall) !!
 
-                        if (validateLogin(username, password)) {
-                            // Inicio de sesión exitoso, redirigir a la siguiente actividad
-                            val intent = Intent(this, LoginInicio::class.java)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            // Mostrar cuadro de diálogo con mensaje de error
-                            val dialogBuilder = AlertDialog.Builder(this)
-                            dialogBuilder.setMessage("Datos ingresados incorrectos.")
-                                .setCancelable(false)
-                                .setPositiveButton("Aceptar") { dialog, _ ->
-                                    dialog.dismiss()
-                                }
+                callableStatement.setString(1, username)
+                callableStatement.setString(2, password)
+                callableStatement.registerOutParameter(3, Types.INTEGER)
+                callableStatement.registerOutParameter(4, Types.INTEGER)
 
-                            val alert = dialogBuilder.create()
-                            alert.setTitle("Error")
-                            alert.show()
-                        }
-                    }
+                callableStatement.execute()
+
+                val abrirVentana = callableStatement.getInt(3)
+                val acceso = callableStatement.getInt(4)
+
+                if (acceso == 1) {
+                    Toast.makeText(this, "Inicio de sesión exitosa", Toast.LENGTH_SHORT).show()
+                    val intent: Intent = Intent(this@Login, docsapoyo::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                }
+                callableStatement.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                conexionSQL?.close()
             }
 
-            private fun validateLogin(username: String, password: String): Boolean {
-                var connection: Connection? = null
-
-                val serverName = "192.168.0.9:50535"
-                val dbName = "dbMindLink"
-                val username = "sa"
-                val password = "user1"
-
-                try {
-                    // Establecer la conexión a la base de datos SQL Server
-                    Class.forName("net.sourceforge.jtds.jdbc.Driver")
-                    val url = "jdbc:jtds:sqlserver://$serverName/$dbName"
-                    connection = DriverManager.getConnection(url, username, password)
-
-                    // Realizar la consulta para validar los datos de inicio de sesión
-                    val query = conexionSQL.dbConn()?.prepareStatement("EXEC PDLogear(?,?")!!
-                    query.setString(1, txtUsuario.text.toString())
-                    query.setString(2, txtContraseña.text.toString())
-                    query.executeQuery()
-
-
-                } catch (e: SQLException) {
-                    e.printStackTrace()
-                } finally {
-                    // Cerrar la conexión
-                    try {
-                        connection?.close()
-                    } catch (e: SQLException) {
-                        e.printStackTrace()
-                    }
-                }
-
-                return false // En caso de cualquier error o si no se encontraron registros
-
-                 //no estoy seguro de esto
-
-
-            */
-
+        }
     }
 }
